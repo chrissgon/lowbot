0fb25431aa41999e99db61a58ffa7637e7d2fe15
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-06-18 17:16:51 -0300

    refactor: :hammer: export standard actions

--- actions.go
+++ actions.go
@@ -7,14 +7,14 @@ import (
 type ActionsMap map[string]func(flow *Flow, channel Channel) (bool, error)
 
 var actions = ActionsMap{
-	"Audio":    actionAudio,
-	"Button":   actionButton,
-	"Document": actionDocument,
-	"Image":    actionImage,
-	"Input":    actionInput,
-	"Text":     actionText,
-	"Video":    actionVideo,
-	"Wait":     actionWait,
+	"Audio":    ActionAudio,
+	"Button":   ActionButton,
+	"Document": ActionDocument,
+	"Image":    ActionImage,
+	"Input":    ActionInput,
+	"Text":     ActionText,
+	"Video":    ActionVideo,
+	"Wait":     ActionWait,
 }
 
 func SetCustomActions(custom ActionsMap) {
@@ -63,48 +63,48 @@ func runActionError(flow *Flow, channel Channel) (bool, error) {
 	return action(flow, channel)
 }
 
-func actionAudio(flow *Flow, channel Channel) (bool, error) {
+func ActionAudio(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendAudio(NewInteractionMessageAudio(flow.SessionID, step.Parameters.Audio, ParseTemplate(step.Parameters.Texts)))
 	return true, err
 }
 
-func actionButton(flow *Flow, channel Channel) (bool, error) {
+func ActionButton(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendButton(NewInteractionMessageButton(flow.SessionID, step.Parameters.Buttons, ParseTemplate(step.Parameters.Texts)))
 	return false, err
 }
 
-func actionDocument(flow *Flow, channel Channel) (bool, error) {
+func ActionDocument(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendDocument(NewInteractionMessageDocument(flow.SessionID, step.Parameters.Document, ParseTemplate(step.Parameters.Texts)))
 	return true, err
 }
 
-func actionImage(flow *Flow, channel Channel) (bool, error) {
+func ActionImage(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendImage(NewInteractionMessageImage(flow.SessionID, step.Parameters.Image, ParseTemplate(step.Parameters.Texts)))
 	return true, err
 }
 
-func actionInput(flow *Flow, channel Channel) (bool, error) {
+func ActionInput(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendText(NewInteractionMessageText(flow.SessionID, ParseTemplate(step.Parameters.Texts)))
 	return false, err
 }
 
-func actionText(flow *Flow, channel Channel) (bool, error) {
+func ActionText(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendText(NewInteractionMessageText(flow.SessionID, ParseTemplate(step.Parameters.Texts)))
 	return true, err
 }
 
-func actionVideo(flow *Flow, channel Channel) (bool, error) {
+func ActionVideo(flow *Flow, channel Channel) (bool, error) {
 	step := flow.Current
 	err := channel.SendVideo(NewInteractionMessageVideo(flow.SessionID, step.Parameters.Video, ParseTemplate(step.Parameters.Texts)))
 	return true, err
 }
 
-func actionWait(flow *Flow, channel Channel) (bool, error) {
+func ActionWait(flow *Flow, channel Channel) (bool, error) {
 	return false, nil
 }
