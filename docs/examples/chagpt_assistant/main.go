b02586989429d561d0b3f6b4f6073c96bde79c77
Christopher Goncalves <chrissgon.dev@gmail.com>
2024-06-25 21:02:00 +0100

    :bug: fix: fix chatgpt consumer

--- docs/examples/chagpt_assistant/main.go
+++ docs/examples/chagpt_assistant/main.go
@@ -4,7 +4,6 @@ import (
 	"os"
 
 	"github.com/chrissgon/lowbot"
-	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
@@ -12,7 +11,7 @@ func main() {
 	channel, _ := lowbot.NewTelegramChannel(os.Getenv("TELEGRAM_TOKEN"))
 
 	// make consumer
-	consumer, _ := lowbot.NewChatGPTConsumer(os.Getenv("CHATGPT_TOKEN"), openai.GPT3Dot5Turbo)
+	consumer, _ := lowbot.NewChatGPTAssistantConsumer(os.Getenv("CHATGPT_TOKEN"), os.Getenv("CHATGPT_ASSISTANT"))
 
 	// start bot
 	lowbot.StartConsumer(consumer, []lowbot.IChannel{channel})
